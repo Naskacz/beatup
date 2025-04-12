e6e9105ca429a2a3383ea40f1e7e33bd1e755f01
Naskacz <milosz.hart@gmail.com>
2025-04-12 19:12:07 +0200

    Initial commit

---
+++ SFMLTemplate/SFMLTemplate.cpp
@@ -0,0 +1,31 @@
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+
+void handleEvent(sf::RenderWindow* window)
+{
+    sf::Event event;
+    while (window->pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+        {
+            window->close();
+        }
+    }
+}
+
+int main()
+{
+    sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(1920, 1080), "CMake SFML Project");
+    window->setFramerateLimit(144);
+
+    while (window->isOpen())
+    {
+        handleEvent(window);
+
+        window->clear(sf::Color::Red);
+        window->display();
+    }
+
+    delete window;
+    return 0;
+}
